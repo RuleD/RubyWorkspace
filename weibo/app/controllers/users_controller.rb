c0aa4a4554bdf9bd8137fa33ee8325c35594b836
724153792@qq.com <root@localhost.localdomain>
2016-12-07 15:13:06 +0800

    add weibo

---
+++ weibo/app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+    @user=User.new
+  end
+
+  def create
+    @user=User.new(users_params)
+    if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def users_params
+    params.require(:user).permit(:userName,:email,:password,:password_confirmation)
+  end
+
+  def show
+    if logged_in?
+      @user=User.find(params[:id])
+    else
+      redirect_to root_url
+    end
+  end
+end
---
+++ weibo/app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  has_many :posts
+
+  validates :userName, presence: true, length: {maximum: 20}
+  EMAIL=/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255}, format: {with: EMAIL},uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, length: {minimum: 6}
+end
