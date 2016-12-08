716547b6b0bb91dda61b100ac68c4af9214bbd4f
RuleD <724153792@qq.com>
2016-12-08 10:06:00 +0800

    remove

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208010054) do
+ActiveRecord::Schema.define(version: 20161207135751) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,14 +26,6 @@ ActiveRecord::Schema.define(version: 20161208010054) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
-  end
-
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "price"
   end
 
 end
