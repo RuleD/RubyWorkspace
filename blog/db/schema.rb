63616cdc7570c371f7b1f5a28817b432ae8f55ba
RuleD <724153792@qq.com>
2016-12-08 15:43:30 +0800

    create user and product

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207135751) do
+ActiveRecord::Schema.define(version: 20161208021613) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,22 @@ ActiveRecord::Schema.define(version: 20161207135751) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "part_number"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
